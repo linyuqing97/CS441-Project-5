f35ce88fb3dc75e78a2b3722297ba5281ddfeedd
j <j@DESKTOP-M0RSPA0>
2019-07-12 21:29:42 -0400

    add basic layout

---
+++ app/src/main/java/com/example/cs441_project_5/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cs441_project_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
