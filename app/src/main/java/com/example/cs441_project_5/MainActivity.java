8765dd13232160ec6ffb15775b32e2dc72df8e57
Yuqing Lin <linyuqing97@gmail.com>
2019-07-16 14:51:18 -0400

    added swipe to delete and save data method

--- app/src/main/java/com/example/cs441_project_5/MainActivity.java
+++ app/src/main/java/com/example/cs441_project_5/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.cs441_project_5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadData();
+
         recyclerView = findViewById(R.id.recycleView);
         addButton = findViewById(R.id.addButton);
 
@@ -48,12 +59,17 @@ public class MainActivity extends AppCompatActivity {
 
 
         editText = findViewById(R.id.editText);
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER);
         totalTV = findViewById(R.id.textTotal);
+        totalTV.setText(String.valueOf(sum()));
 
 
+
+
+
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
         myAdapter = new recycleAdapter(input);
         recyclerView.setAdapter(myAdapter);
-
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +96,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    public void onPause(){
+        super.onPause();
+        saveData();
+    }
+
+
     public int sum(){
         total = 0;
         for(int i = 0;i<input.size();i++){
@@ -87,5 +109,42 @@ public class MainActivity extends AppCompatActivity {
         }
         return total;
     }
+    private void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(input);
+        editor.putString("Input list",json);
+        editor.apply();
+    }
+    private void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences",MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("Input list",null);
+        Type type = new TypeToken<ArrayList<String>>(){}.getType();
+        input = gson.fromJson(json,type);
+
+        if(input == null){
+            input= new ArrayList<>();
+        }
+
+
+    }
+
+
+
+    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.RIGHT|ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            input.remove(viewHolder.getAdapterPosition());
+            myAdapter.notifyDataSetChanged();
+            totalTV.setText(String.valueOf(sum()));
+        }
+    };
 
 }
